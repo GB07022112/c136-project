5d78ad83b19961ff07cd05099e7de660533a46bc
GB07022112 <128438908+GB07022112@users.noreply.github.com>
2023-05-20 17:24:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+status = "";
+
+function setup()
+{
+    canvas.createCanvas(200,200);
+    canvas.center();
+    video.createCapture(200,200);
+    video.hide();
+}
+
+function start()
+{
+    
+}
